840b5ae93435b0e755006ca0e106579daf14e3e7
Aviral <aviral.jain2020@vitstudent.ac.in>
2022-06-06 20:46:44 +0530

    modified POST

--- Files/product/routes.py
+++ Files/product/routes.py
@@ -28,15 +28,13 @@ resource_fields = {
     'related_products': fields.String,
 }
 
-@product.route('/add', methods=['POST'])
-
 @product.get('/')
 @marshal_with(resource_fields)
 def get_products():
-    result=db.session.query(Product).all()
-    return json.dumps(result)
+    result=Product.query.all()
+    return jsonify(result)
 
-@product.get('/<id>')
+@product.get('/<int:id>')
 @marshal_with(resource_fields)
 def get_product(id):
     result=db.session.query(Product).filter(Product.product_id==id).first()
@@ -44,24 +42,36 @@ def get_product(id):
         abort(404, message="Product {} doesn't exist".format(id))
     return jsonify(result)
 
+# @product.post('/')
+# @marshal_with(resource_fields)
+# def add_product():
+#     if request.is_json:
+#         args=add_product_args.parse_args()
+#         product_name=args['name']
+#         product_description=args['description']
+#         product_price=args['price']
+#         product_image=args['image']
+#         product_discount=args['discount']
+#         product_qty=args['qty_left']
+#         product_category=args['category']
+#         product_related_products=args['related_products']
+#         product=Product(name=product_name,description=product_description,price=product_price,
+#             image=product_image,discount=product_discount,effective_price=product_price-(product_discount*product_price/100),
+#             qty_left=product_qty,category=product_category,related_products=product_related_products)
+#         db.session.add(product)
+#         db.session.commit()
+#         return product, 201
+
+#     return {"error": "Request must be JSON"}, 415
+
 @product.post('/')
 @marshal_with(resource_fields)
 def add_product():
     if request.is_json:
-        args=add_product_args.parse_args()
-        product_name=args['name']
-        product_description=args['description']
-        product_price=args['price']
-        product_image=args['image']
-        product_discount=args['discount']
-        product_qty=args['qty_left']
-        product_category=args['category']
-        product_related_products=args['related_products']
-        product=Product(name=product_name,description=product_description,price=product_price,
-            image=product_image,discount=product_discount,effective_price=product_price-(product_discount*product_price/100),
-            qty_left=product_qty,category=product_category,related_products=product_related_products)
+        product=Product(name=request.json['name'],description=request.json['description'],price=request.json['price'],
+            image=request.json['image'],discount=request.json['discount'],effective_price=float(request.json['price'])-(float(request.json['discount'])*float(request.json['price'])/100),
+            qty_left=request.json['qty_left'],category=request.json['category'],related_products=request.json['related_products'])
         db.session.add(product)
         db.session.commit()
         return product, 201
-
     return {"error": "Request must be JSON"}, 415
