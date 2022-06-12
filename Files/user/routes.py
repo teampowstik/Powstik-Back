8979c835c8280689190757ab2169ece7152498ae
ash-02 <ashwins.2019@vitstudent.ac.in>
2022-06-13 02:43:43 +0530

    removed one post user route and added update user details and change password function

--- Files/user/routes.py
+++ Files/user/routes.py
@@ -1,91 +1,56 @@
 from email.policy import HTTP
-from unittest import result
 from flask import Blueprint, jsonify, request
-from .utils import add_new_user, retrieve_all_users, retrieve_user_byID, remove_user, update_user
-from ..models import User, UserSchema
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_jwt_extended import create_access_token, create_refresh_token
-from Files import db
-
+from sqlalchemy import false, true
+from .utils import change_password, login_user, register_user, retrieve_all_users, retrieve_user_byID, remove_user, update_user
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
 @user.post('/register')
 def register():
-    password = request.json.get('password')
-    pwd_hash = generate_password_hash(password)
-
-    user=User(first_name=request.json['first_name'],last_name=request.json['last_name'],
-                email=request.json['email'],password = pwd_hash,
-                phone = request.json['phone'],user_type = request.json['user_type'])
+    if request.is_json:
+        first_name = request.json.get('first_name')
+        last_name = request.json.get('last_name')
+        email = request.json.get('email')
+        password = request.json.get('password')
+        phone = request.json.get('phone')
+        user_type = request.json.get('user_type')
+        result = register_user(first_name, last_name, email, password, phone, user_type)
+        return result
+    return {"message": "Request must be JSON"}, 415
     
-    db.session.add(user)
-    db.session.commit()
-    # return {"message": "User Successfully Added"}, 201
-    return jsonify({
-        'message': 'User Successfully Added',
-        'password': pwd_hash,
-        'old password': password
-    }), 201
-
-
 @user.post('/login')
 def login():
     email = request.json.get('email')
     password = request.json.get('password')
 
-    user=db.session.query(User).filter(User.email==email).first()
-    user_schema=UserSchema()
-    output = user_schema.dump(user)
+    result = login_user(email, password)
 
-    if output:
-        is_pass_correct = check_password_hash(output["password"], password)
+    if result is true:
+        return result
 
-        if is_pass_correct:
-            refresh = create_refresh_token(identity=output["user_id"])
-            access = create_access_token(identity=output["user_id"])
+    return result, 404
 
-            return jsonify({
-                'user': {
-                    'refresh': refresh,
-                    'access': access,
-                    'email': output["email"],
-                    'entered password' : password,
-                    'hashed passord' : output["password"],
-                }
-            })
-
-        return jsonify("Incorrect Password"), 401
-
-    return jsonify("User Credentials Incorrect"), 404
-
-@user.post('/')
-def post_user():
+@user.patch('/change password/<int:user_id>')
+def patch_user_password(user_id):
     if request.is_json:
-        first_name = request.json.get('first_name')
-        last_name = request.json.get('last_name')
-        email = request.json.get('email')
-        password = request.json.get('password')
-        phone = request.json.get('phone')
-        user_type = request.json.get('user_type')
-        add_new_user(first_name, last_name, email, password, phone, user_type)
-        return {"message": "User Successfully Added"}, 201
+        old_password = request.json.get('old_password')
+        new_password = request.json.get('new_password')
+        result = change_password(user_id, old_password, new_password)
+        return result
     return {"message": "Request must be JSON"}, 415
-    # return add_new_user()
 
-@user.patch('/<int:user_id>')
-def patch_user(user_id):
+@user.patch('/user details/<int:user_id>')
+def patch_user_details(user_id):
     if request.is_json:
         first_name = request.json.get('first_name')
         last_name = request.json.get('last_name')
         email = request.json.get('email')
         password = request.json.get('password')
         phone = request.json.get('phone')
-        user_type = request.json.get('user_type')
-        res=update_user(user_id, first_name, last_name, email, password, phone, user_type)
+        res = update_user(user_id, first_name, last_name, email, password, phone)
         if res is None:
             return {"message": "User not found"}, 404
-        return {"message": "User Successfully Updated"}, 200
+        return res
     return {"message": "Request must be JSON"}, 415
 
 @user.get('/')
