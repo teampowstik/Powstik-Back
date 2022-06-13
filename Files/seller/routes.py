54adeaca92fb228811e4137dc7cee315330cb303
Aviral <aviral.jain2020@vitstudent.ac.in>
2022-06-14 01:24:50 +0530

    updated seller

--- Files/seller/routes.py
+++ Files/seller/routes.py
@@ -1,110 +1,77 @@
 from email.policy import HTTP
-from unittest import result
 from flask import Blueprint, jsonify, request
-from .utils import add_new_user, retrieve_all_users, retrieve_user_byID, remove_user, update_user
-from ..models import User, UserSchema, Seller, SellerSchema
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_jwt_extended import create_access_token, create_refresh_token
-from Files import db
+from sqlalchemy import false, true
+from .utils import change_password, login_user, register_user, retrieve_all_users, retrieve_user_byID, remove_user, update_user
 
+seller = Blueprint('seller', __name__, url_prefix='/seller')
 
-user = Blueprint('user', __name__, url_prefix='/seller')
-
-@user.post('/register')
+@seller.post('/register')
 def register():
-    password = request.json.get('password')
-    pwd_hash = generate_password_hash(password)
-
-    user=User(first_name=request.json['first_name'],last_name=request.json['last_name'],
-                email=request.json['email'],password = pwd_hash,
-                phone = request.json['phone'],user_type = request.json['user_type'])
-    seller=Seller(shop_name=request.json['shop_name'],shop_url=request.json['shop_url'])
+    if request.is_json:
+        first_name = request.json.get('first_name')
+        last_name = request.json.get('last_name')
+        email = request.json.get('email')
+        password = request.json.get('password')
+        phone = request.json.get('phone')
+        user_type = request.json.get('user_type')
+        shop_name = request.json.get('shop_name')
+        shop_url = request.json.get('shop_url')
+        result = register_user(first_name, last_name, email, password, phone, user_type, shop_name, shop_url)
+        return result
+    return {"message": "Request must be JSON"}, 415
     
-    db.session.add(user)
-    db.session.add(seller)
-    db.session.commit()
-    # return {"message": "User Successfully Added"}, 201
-    return jsonify({
-        'message': 'Seller Successfully Added',
-        'password': pwd_hash,
-        'old password': password
-    }), 201
-
-
-@user.post('/login')
+@seller.post('/login')
 def login():
     email = request.json.get('email')
     password = request.json.get('password')
 
-    user=db.session.query(User).filter(User.email==email).first()
-    user_schema=UserSchema()
-    output = user_schema.dump(user)
+    result = login_user(email, password)
 
-    if output:
-        is_pass_correct = check_password_hash(output["password"], password)
+    if result is true:
+        return result
 
-        if is_pass_correct:
-            refresh = create_refresh_token(identity=output["user_id"])
-            access = create_access_token(identity=output["user_id"])
+    return result, 404
 
-            return jsonify({
-                'user': {
-                    'refresh': refresh,
-                    'access': access,
-                    'email': output["email"],
-                    'entered password' : password,
-                    'hashed passord' : output["password"],
-                }
-            })
-
-        return jsonify("Incorrect Password"), 401
-
-    return jsonify("User Credentials Incorrect"), 404
-
-@user.post('/')
-def post_user():
+@seller.patch('/change_password/<int:user_id>')
+def patch_user_password(user_id):
     if request.is_json:
-        first_name = request.json.get('first_name')
-        last_name = request.json.get('last_name')
-        email = request.json.get('email')
-        password = request.json.get('password')
-        phone = request.json.get('phone')
-        user_type = request.json.get('user_type')
-        add_new_user(first_name, last_name, email, password, phone, user_type)
-        return {"message": "User Successfully Added"}, 201
+        old_password = request.json.get('old_password')
+        new_password = request.json.get('new_password')
+        result = change_password(user_id, old_password, new_password)
+        return result
     return {"message": "Request must be JSON"}, 415
-    # return add_new_user()
 
-@user.patch('/<int:user_id>')
-def patch_user(user_id):
+@seller.patch('/<int:user_id>')
+def patch_user_details(user_id):
     if request.is_json:
         first_name = request.json.get('first_name')
         last_name = request.json.get('last_name')
         email = request.json.get('email')
         password = request.json.get('password')
         phone = request.json.get('phone')
-        user_type = request.json.get('user_type')
-        res=update_user(user_id, first_name, last_name, email, password, phone, user_type)
+        shop_name = request.json.get('shop_name')
+        shop_url = request.json.get('shop_url')
+        res = update_user(user_id, first_name, last_name, email, password, phone, shop_name, shop_url)
         if res is None:
             return {"message": "User not found"}, 404
-        return {"message": "User Successfully Updated"}, 200
+        return res
     return {"message": "Request must be JSON"}, 415
 
-@user.get('/')
+@seller.get('/')
 def get_users():
     result = retrieve_all_users()
     if result is None:
         return jsonify({'message': 'No users found'}), 404
     return jsonify(result)
 
-@user.get('/<int:user_id>')
+@seller.get('/<int:user_id>')
 def get_user_byID(user_id):
     result = retrieve_user_byID(user_id)
     if result is None:
         return jsonify({'message': 'No user found'}), 404
-    return jsonify(result)
+    return result
 
-@user.delete("/<int:user_id>")
+@seller.delete("/<int:user_id>")
 def delete_user(user_id):
     result = remove_user(user_id)
     if result is None:
--- Files/seller/utils.py
+++ Files/seller/utils.py
@@ -1,45 +1,105 @@
 from flask import request, jsonify
 from Files import db
-from ..models import User, UserSchema
+from ..models import User, Seller, UserSchema, SellerSchema
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_jwt_extended import create_access_token, create_refresh_token
+from sqlalchemy import or_
 
-def add_new_user(first_name, last_name, email, password, phone, user_type):
-    if request.is_json:
-        user=User(first_name=first_name,last_name=last_name,email=email,password=password,phone=phone,user_type=user_type)
-        db.session.add(user)
-        db.session.commit()
+def login_user(email, password):
+    user=db.session.query(User).filter(User.email==email).first()
+    user_schema=UserSchema()
+    output = user_schema.dump(user)
 
+    if output:
+        is_pass_correct = check_password_hash(output["password"], password)
+
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=output["user_id"])
+            access = create_access_token(identity=output["user_id"])
+
+            return jsonify({"status" : "logged in" , "access_token": access, "refresh_token": refresh})
+        return "Incorrect Password"
+
+    return "User Credentials Incorrect"
+
+
+def register_user(first_name, last_name, email, password, phone, user_type, shop_name, shop_url):
+
+    user=db.session.query(User).filter(or_(User.email==email,User.phone==phone)).first()
+    user_schema=UserSchema()
+    output = user_schema.dump(user)
+
+    if output:
+        return {"message": "Phone or Email already exists"}, 409
+    
+    password = request.json.get('password')
+    pwd_hash = generate_password_hash(password)
+
+    user=User(first_name=first_name,last_name=last_name, email=email,password = pwd_hash, phone = phone, user_type = user_type)
+    db.session.add(user)
+    user_id=db.session.query(User).filter(User.email==email).first().user_id
+    seller=Seller(shop_name=shop_name, shop_url=shop_url, seller_id=user_id)
+    db.session.add(seller)
+    db.session.commit()
+    return {"message" : "Seller Added"}, 201
+    
 def retrieve_all_users():
-    user_details = User.query.all()
-    user_schema=UserSchema(many=True)
-    output = user_schema.dump(user_details)
-    return output
+    #display all sellers alongwith information from user table and seller table
+    user_details = db.session.query(User,Seller).join(Seller,User.user_id==Seller.seller_id).all()
+
 
 def retrieve_user_byID(user_id):
     user_details=db.session.query(User).filter(User.user_id==user_id).first()
-    if not user_details:
+    seller_details=db.session.query(Seller).filter(Seller.seller_id==user_id).first()
+    if not user_details or not seller_details:
         return {"message": "User not found"}, 404
     user_schema=UserSchema()
-    output = user_schema.dump(user_details)
-    return output
+    output_user = user_schema.dump(user_details)
+    seller_schema=SellerSchema()
+    output_seller = seller_schema.dump(seller_details)
+    return jsonify({"user": output_user, "seller": output_seller})
 
 def remove_user(user_id):
     user_details=db.session.query(User).filter(User.user_id==user_id).first()
-    if not user_details:
+    seller_details=db.session.query(Seller).filter(Seller.seller_id==user_id).first()
+    if not user_details or not seller_details:
         return None
     db.session.delete(user_details)
+    db.session.delete(seller_details)
     db.session.commit()
     return {"message": "User Successfully deleted"}, 201
 
-def update_user(user_id, first_name, last_name, email, password, phone, user_type):
-    user_details=db.session.query(User).filter(User.user_id==user_id).first()
-    if not user_details:
-        return None
-    user_details.first_name=first_name
-    user_details.last_name=last_name
-    user_details.email=email
-    user_details.password=password
-    user_details.phone=phone
-    user_details.user_type=user_type
+def update_user(user_id, first_name, last_name, email, password, phone, shop_name, shop_url):
+    user=db.session.query(User).filter(User.user_id==user_id).first()
+    seller=db.session.query(Seller).filter(Seller.seller_id==user_id).first()
+    user_schema=UserSchema()
+    output = user_schema.dump(user)
 
-    db.session.commit()
-    return {"message": "User Successfully updated"}, 201
+    is_pass_correct = check_password_hash(output["password"], password)
+
+    if is_pass_correct:
+        user.first_name=first_name
+        user.last_name=last_name
+        user.email=email
+        user.phone=phone
+        seller.shop_name=shop_name
+        seller.shop_url=shop_url
+
+        db.session.commit()
+        return {"message": "User Successfully updated"}, 201        
+    
+    return "Incorrect Password"
+
+def change_password(user_id, old_password, new_password):
+    user=db.session.query(User).filter(User.user_id==user_id).first()
+    user_schema=UserSchema()
+    output = user_schema.dump(user)
+
+    is_pass_correct = check_password_hash(output["password"], old_password)
+
+    if is_pass_correct:
+        user.password = generate_password_hash(new_password)
+        db.session.commit()
+        return {"message": "User Password Successfully changed"}, 201        
+
+    return "Incorrect Password"
