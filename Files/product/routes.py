50a899195512588b477ab06aebc80e36e23a2059
Aviral <aviral.jain2020@vitstudent.ac.in>
2022-07-06 00:11:28 +0530

    user register fixed

--- Files/product/routes.py
+++ Files/product/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, request
 from .utils import get_all_products, get_product_by_id, products_by_category, add_product, update_product, remove_product
 from flask_jwt_extended import jwt_required, get_jwt_identity
-
+from flask_cors import cross_origin, CORS
 
 product = Blueprint('product', __name__, url_prefix='/product')
+cors = CORS(product, resources={r"/foo": {"origins": "*"}})
 
 @product.get('/')
 def get_products():
@@ -26,14 +27,15 @@ def get_product(id):
     return (result), 200
 
 @product.get('/bycategory/<string:category_name>')
+@cross_origin(origin='*',headers=['Content- Type','Authorization'])
 def get_product_by_category_name(category_name):
     result = products_by_category(category_name)
     if result is None:
         response={}
-        response.headers.add("Access-Control-Allow-Origin", "*")
+        # response.headers.add("Access-Control-Allow-Origin", "*")
         return response, 204
     response =  jsonify({"result": result})
-    response.headers.add("Access-Control-Allow-Origin", "*")
+    # response.headers.add("Access-Control-Allow-Origin", "*")
     return response, 200
 
 @product.post('/')
